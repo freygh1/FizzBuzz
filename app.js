9cedcff03b9fa917ed7442b07c38b96f677452d3
freygh1 <freilinshernandez10@gmail.com>
2025-08-12 13:11:55 -0400

    Accessed to the HTML elements by their ID

---
+++ app.js
@@ -0,0 +1,18 @@
+//PseudoCode
+
+/**
+ * 1. The user needs to insert a number. That number need to be
+ *    greater than zero and equal or less than one hundred.
+ * 2. To show the results, the user needs to press the buttom "play".
+ * 3. When the buttom would be pressed, the screen should show the results.
+ * 4. The results should be an amount of numbers between 1 and the number that the
+ *    user entered in the input. The numbers that are divisible by 3, need to be
+ *    replace by "Fizz" and the numbers that are divisible by 5, need to be
+ *    replace by "Buzz".
+ **/
+
+// accessing to the elements
+
+const inputValue = document.getElementById('input-value')
+const btn = document.getElementById('btn')
+const resultCtn = document.getElementById('btn')
